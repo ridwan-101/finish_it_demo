671de2192c8478a72249985745a76261c7803b53
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-08-24 16:46:47 +0100

    landing page 🚀🔥

--- lib/ui/landingpage.dart
+++ lib/ui/landingpage.dart
@@ -11,53 +11,90 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(
-            'images/upercurved.png',
-            fit: BoxFit.none,
-            alignment: Alignment.bottomCenter,
-          ),
-          const SizedBox(height: 60),
-          Image.asset(
-            'images/logo.png',
-            fit: BoxFit.none,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            "Welcome to Finish it",
-            style: TextStyle(
-              fontSize: 32,
-              height: 1.2,
-              fontFamily: "Lato",
-              fontWeight: FontWeight.w500,
-              color: Color(0xFF1BA371),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/upercurved.png',
+              fit: BoxFit.none,
+              alignment: Alignment.bottomCenter,
             ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          const Text(
-            "Find recipes based on the ingredients you have",
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            height: 370,
-            width: 350,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "What ingredients do you want to finish today ",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
+            const SizedBox(height: 60),
+            Image.asset(
+              'images/logo.png',
+              fit: BoxFit.none,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Welcome to Finish it",
+              style: TextStyle(
+                fontSize: 32,
+                height: 1.2,
+                fontFamily: "Lato",
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF1BA371),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "Find recipes based on the ingredients you have",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 345,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "What ingredients do you want to finish today ? ",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
                 ),
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 60,
+            ),
+            SizedBox(
+              width: 138,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF1BA371),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(1),
+              width: 427,
+              height: 447,
+              child: Image.asset(
+                'images/bcurved.png', // Replace with your image path
+                fit: BoxFit.none,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
