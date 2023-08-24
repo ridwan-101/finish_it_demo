fd5be1900abb797e774b0ad8fa2ede31a18cc882
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-08-24 14:01:36 +0100

    landing page 📍

--- lib/ui/landingpage.dart
+++ lib/ui/landingpage.dart
@@ -10,6 +10,38 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset(
+            'images/upercurved.png',
+            fit: BoxFit.none,
+            alignment: Alignment.bottomCenter,
+          ),
+          const SizedBox(height: 60),
+          Image.asset(
+            'images/logo.png',
+            fit: BoxFit.none,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            "Welcome to Finish it",
+            style: TextStyle(
+              fontSize: 32,
+              height: 1.2,
+              fontFamily: "Lato",
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF1BA371),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text("Find recipes based on the ingredients you have")
+        ],
+      ),
+    );
   }
 }
