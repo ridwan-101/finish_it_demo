305c015c52f0ab2f67ad6dea210ff465d165aa94
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-08-31 13:16:43 +0100

    event class 🔗✅

--- lib/features/search result/bloc/search_result_event.dart
+++ lib/features/search result/bloc/search_result_event.dart
@@ -2,3 +2,13 @@ part of 'search_result_bloc.dart';
 
 @immutable
 abstract class SearchResultEvent {}
+
+class FavoriteRecepieButttonClickedEvent {}
+
+class FavoriteRecepieButttonNavigateEvent {}
+
+class RefreshResultsClickedEvent {}
+
+class ViewRecepieClickedEvent {}
+
+class ViewRecepieClickedNavigateEvent {}
