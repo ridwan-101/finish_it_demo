7e117c92e4062db88ce03fbc58654b24523eb968
ridwan-101 <abdulazeezridwan004@gmail.com>
2023-08-31 20:32:30 +0100

    👾👾

--- lib/features/search result/ui/recipelistresult.dart
+++ lib/features/search result/ui/recipelistresult.dart
@@ -1,3 +1,4 @@
+import 'package:finish_it_demo/ui/landingpage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,8 +18,8 @@ class _RecipeListResultState extends State<RecipeListResult> {
   Widget build(BuildContext context) {
     return BlocConsumer<SearchResultBloc, SearchResultState>(
       bloc: searchResultBloc, //parameter SearchResultBloc
-      listenWhen: (previous, current) {},
-      buildWhen: (previous, current) {},
+      // listenWhen: (previous, current) {},
+      // buildWhen: (previous, current) {},
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
@@ -35,8 +36,48 @@ class _RecipeListResultState extends State<RecipeListResult> {
             leading: IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.black),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const LandingPage()), // Replace NewPage with your actual page widget
+                );
               },
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    // Refresh logic here
+                  },
+                  child: Container(
+                    width: 150,
+                    height: 51,
+                    margin: const EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF018354),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Refresh results",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Icon(Icons.sync_outlined, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         );
